501fcb7d60717401345ca8df718659233c6b95f4
alejandrodiazpinilla <diazjohn83@gmail.com>
2019-07-09 14:25:57 -0500

    solucion make

--- solution.rb
+++ solution.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 get '/' do 
-    enca = env['HTTP_USER_AGENT']
+    enca = env['HTTP_USER_AGENT'] # tambien se puede de esta manera request.env["HTTP_USER_AGENT"]
     "#{enca}" 
 end
 
